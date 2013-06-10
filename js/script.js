9ae32c7b7be07e0315912d7f6918adb3114b363f
Michiel Vancoillie <info@ninetynine.be>
2013-06-10 18:08:43 +0200

    Save power state on sleep

--- js/script.js
+++ js/script.js
@@ -1,70 +1,105 @@
 var hostname = '';
+var alias = '';
 
 window.Jobs = (function() {
 
-	/*
-	 * Execute all functions for passed config
-	 */
-	function add(job) {
-		var str = job.minutes + " " + job.hours + " " + job.day_of_month + " " + job.month + " " + job.day_of_week;
+    /*
+     * Execute all functions for passed config
+     */
+    function add(job) {
+        var str = job.minutes + " " + job.hours + " " + job.day_of_month + " " + job.month + " " + job.day_of_week;
 
-		//parse the string into a scheduler
-		var scheduler = cronParser().parse(str, false);
+        //parse the string into a scheduler
+        var scheduler = cronParser().parse(str, false);
 
-		//execute a job every next time
-		var thisjob = later(1, true); //1 second = min time between 2 occurences, true => use Local time
-		thisjob.exec(scheduler, new Date(), execute, job);
-	}
+        //execute a job every next time
+        var thisjob = later(1, true); //1 second = min time between 2 occurences, true => use Local time
+        thisjob.exec(scheduler, new Date(), execute, job);
+    }
 
-	function execute(job){
-		try {
-			eval(job.javascript);
-		} catch(err) {
-			//do nothing
-		}
-	}
+    function execute(job){
+        try {
+            eval(job.javascript);
+        } catch(err) {
+            //do nothing
+        }
+    }
 
-	/*
-	 * Public interface to this object
-	 */
-	return {
-		add : add
-	};
+    /*
+     * Public interface to this object
+     */
+    return {
+        add : add
+    };
 
 }());
 
 var Power = {
-	enable : function() {
-		if (typeof application == "object")
-			application.enableScreen(true);
-		console.log('enable');
-		document.location.href = 'https://go.flatturtle.com/' + hostname;
-	},
+    enable : function() {
+        saveState('on');
+        if (typeof application == "object")
+            application.enableScreen(true);
+        console.log('enable');
+        document.location.href = 'https://go.flatturtle.com/' + hostname;
+    },
 
-	disable : function() {
-		if (typeof application == "object")
-			application.enableScreen(false);
-		console.log('disable');
-	}
+    disable : function() {
+        saveState('off');
+        if (typeof application == "object")
+            application.enableScreen(false);
+        console.log('disable');
+    }
 };
 
-function loadConfig(api) {
-	$.ajax({
-		url : api,
-		dataType: "json",
-		success : function(config) {
-			// Create jobs
-			for(var id in config.jobs) {
-				var job = config.jobs[id];
-				Jobs.add(job);
-			}
 
-			// Get hostname
-			if(config.interface.hostname){
-				hostname = config.interface.hostname;
-			}
-		}
-	});
+/**
+ * Save power state
+ */
+function saveState(state){
+    if(alias){
+        $.ajax({
+            url : '/' + alias + '/plugins/screen/power_state',
+            type: 'post',
+            dataType: "json",
+            data: {
+                state: state
+            }
+        });
+    }
 }
 
-Power.disable();
+/**
+ * Load screen configuration
+ */
+function loadConfig(api) {
+    $.ajax({
+        url : api,
+        dataType: "json",
+        success : function(config) {
+
+
+            // Create jobs
+            for(var id in config.jobs) {
+                var job = config.jobs[id];
+                Jobs.add(job);
+            }
+
+            // Get hostname
+            if(config.interface.hostname){
+                hostname = config.interface.hostname;
+            }
+
+            // Get alias
+            if(config.interface.alias){
+                alias = config.interface.alias;
+            }
+
+            // Default disable power
+            Power.disable();
+        }
+    });
+}
+
+// Default disable
+if (typeof application == "object")
+    application.enableScreen(false);
--- sleep.php
+++ sleep.php
@@ -1,43 +1,41 @@
 <?php $rand = rand(); ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title>InfoScreen in Sleep</title>
-		<link rel="stylesheet" href="css/style.css?<?php echo $rand; ?>">
-   		<link type="text/css" rel="stylesheet" href="//fast.fonts.com/cssapi/66253153-9c89-413c-814d-60d3ba0d6ac2.css"/>
-	</head>
-	<body>
-		<?php include_once("ga.php") ?>
-		<!--[if lt IE 7]>
-			<p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
-		<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>InfoScreen in Sleep</title>
+        <link rel="stylesheet" href="css/style.css?<?php echo $rand; ?>">
+        <link type="text/css" rel="stylesheet" href="//fast.fonts.com/cssapi/66253153-9c89-413c-814d-60d3ba0d6ac2.css"/>
+    </head>
+    <body>
 
-		<div class='container'>
-			<h1>Your screen is fast asleep!</h1>
+        <div class='container'>
+            <h1>Your screen is fast asleep!</h1>
           If you can see this page, it means the display didn't turn off correctly.<br />
           Please get in touch with the FlatTurtle helpdesk.<br /><br /><br />
           <footer>
-          	<ul>
+            <ul>
               <li>Helpdesk: <span>+32 (0) 2 669 09 99</span></li>
               <li>E-mail: <span>help@FlatTurtle.com</span></li>
               <li>Web: <span>http://FlatTurtle.com</span></li>
             </ul>
           </footer>
-			<img src='img/ft_logo.jpg' alt='logo FlatTurtle' />
-		</div>
+            <img src='img/ft_logo.jpg' alt='logo FlatTurtle' />
+        </div>
 
-		<script src="js/jquery.js?<?php echo $rand; ?>"></script>
-		<script src="js/later.js?<?php echo $rand; ?>"></script>
-		<script src="js/script.js?<?php echo $rand; ?>"></script>
+        <script src="js/jquery.js?<?php echo $rand; ?>"></script>
+        <script src="js/later.js?<?php echo $rand; ?>"></script>
+        <script src="js/script.js?<?php echo $rand; ?>"></script>
 
-		<script>
-			loadConfig('<?php echo $alias; ?>.json?<?php echo $rand; ?>');
-			window.setTimeout(function(){
-				application.enableScreen(false);
-			},3000); // after 3 seconds
-		</script>
+        <script>
+            loadConfig('<?php echo $alias; ?>.json?<?php echo $rand; ?>');
+            window.setTimeout(function(){
+                if (typeof application == "object")
+                    application.enableScreen(false);
+            },3000); // after 3 seconds
+        </script>
 
-	</body>
+        <?php include_once("ga.php") ?>
+    </body>
 </html>
